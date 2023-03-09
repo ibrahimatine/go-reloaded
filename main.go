97df78deb7c55efd176cf220d4718373a0df9573
itine <ibrahimatine29@gmail.com>
2023-03-09 06:08:09 -0800

    daraaa

--- main.go
+++ main.go
@@ -11,12 +11,19 @@ func conversion(s string) string {
 	chaine := strings.Split(s, " ")
 	for i, mot := range chaine {
 		if strings.HasSuffix(mot, "(hex)") {
-			hx := strings.TrimSuffix(chaine[i-1], "(hex)")
-			dc, err := strconv.ParseInt(hx, 16, 64)
+			dc, err := strconv.ParseInt(chaine[i-1], 16, 64)
 			if err != nil {
 				return s
 			}
-			chaine[i-1] = strconv.FormatInt(dc, 10) // Pour remplacer le int en caractere
+			chaine[i-1] = strconv.FormatInt(dc, 10) //pour convertir les int en alpha
+			chaine[i] = strings.TrimSuffix(mot, "(hex)")
+		} else if strings.HasSuffix(mot, "(bin)") {
+			dc, err := strconv.ParseInt(chaine[i-1], 2, 64)
+			if err != nil {
+				return s
+			}
+			chaine[i-1] = strconv.FormatInt(dc, 10)
+			chaine[i] = strings.TrimSuffix(mot, "(bin)")
 
 		}
 	}
@@ -27,6 +34,6 @@ func conversion(s string) string {
 
 func main() {
 
-	s := "1E (hex) files were added. It has been F (hex) years."
+	s := "1E (hex) files were added. It has been 10 (bin) years."
 	fmt.Println(conversion(s))
 }
