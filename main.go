ef1121c45b61f4514ae428279b979cacc507d3b2
itine <ibrahimatine29@gmail.com>
2023-03-11 03:40:39 -0800

    ok

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
-func conversion(s string) string {
+/*func conversion(s string) string {
 
 	chaine := strings.Split(s, " ")
 	for i, mot := range chaine {
 		if strings.HasSuffix(mot, "(hex)") {
 			dc, err := strconv.ParseInt(chaine[i-1], 16, 64)
-			if err != nil {
+			if err == nil {
 				return s
 			}
 			chaine[i-1] = strconv.FormatInt(dc, 10) //pour convertir les int en alpha
@@ -30,10 +31,55 @@ func conversion(s string) string {
 
 	return strings.Join(chaine, " ")
 
-}
+}*/
 
 func main() {
 
-	s := "1E (hex) files were added. It has been 10 (bin) years."
-	fmt.Println(conversion(s))
+	arguments := os.Args[1:]
+	if len(arguments) == 2 {
+
+		data, err := os.ReadFile("sample.txt")
+		if err == nil {
+			chaine := strings.Split(string(data), " ")
+			nchaine := string(data)
+
+			for i, mot := range chaine {
+
+				if strings.HasSuffix(mot, "(hex)") {
+
+					dc, err := strconv.ParseInt(chaine[i-1], 16, 64)
+					if err == nil {
+						dci := strconv.FormatInt(dc, 10)
+						chaine[i] = strings.TrimSuffix(mot, "(hex)")
+						chaine[i-1] = dci //strings.ReplaceAll(nchaine, chaine[i-1], dci)
+						nchaine = strings.Join(chaine, " ")
+					}
+				} else if strings.HasSuffix(mot, "(bin)") {
+					dc, err := strconv.ParseInt(chaine[i-1], 2, 64)
+					if err == nil {
+						dci := strconv.FormatInt(dc, 10)
+						chaine[i] = strings.TrimSuffix(mot, "(bin)")
+						chaine[i-1] = dci
+						nchaine = strings.Join(chaine, " ")
+					}
+				}
+
+				if strings.HasSuffix(mot, "(up)") || strings.HasSuffix(mot, "(up),") {
+					up := strings.ToUpper(chaine[i-1])
+					chaine[i] = strings.TrimSuffix(mot, "(up)")
+					if strings.HasSuffix(mot, "(up),") {
+						chaine[i] = strings.TrimSuffix(mot, "(up)")
+					}
+
+					chaine[i-1] = up
+					nchaine = strings.Join(chaine, " ")
+
+				}
+
+			}
+			fmt.Println(nchaine)
+		}
+
+	}
+
 }
