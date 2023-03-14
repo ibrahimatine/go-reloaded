6dd4945128553567c79afb3f0637fde2cad81317
Ibrahima  Tine <itine@noreply.learn.zone01dakar.sn>
2023-03-14 06:26:13 +0000

    Update 'main.go'

--- main.go
+++ main.go
@@ -9,32 +9,6 @@ import (
 	"unicode"
 )
 
-/*func conversion(s string) string {
-
-	chaine := strings.Split(s, " ")
-	for i, mot := range chaine {
-		if strings.HasSuffix(mot, "(hex)") {
-			dc, err := strconv.ParseInt(chaine[i-1], 16, 64)
-			if err == nil {
-				return s
-			}
-			chaine[i-1] = strconv.FormatInt(dc, 10) //pour convertir les int en alpha
-			chaine[i] = strings.TrimSuffix(mot, "(hex)")
-		} else if strings.HasSuffix(mot, "(bin)") {
-			dc, err := strconv.ParseInt(chaine[i-1], 2, 64)
-			if err != nil {
-				return s
-			}
-			chaine[i-1] = strconv.FormatInt(dc, 10)
-			chaine[i] = strings.TrimSuffix(mot, "(bin)")
-
-		}
-	}
-
-	return strings.Join(chaine, " ")
-
-}*/
-
 func esp(s string) string {
 
 	re := regexp.MustCompile(`\s+`)
