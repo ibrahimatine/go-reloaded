468ede44d40ca23ea8b7a85ea627b45998c414a4
Ibrahima  Tine <itine@noreply.learn.zone01dakar.sn>
2023-04-06 14:52:31 +0000

    Update 'main.go'

--- main.go
+++ main.go
@@ -2,30 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
 	"unicode"
 )
-
-/*func esp(s string) string {
-	re := regexp.MustCompile(`\s+`)
-	s = re.ReplaceAllString(s, " ")
-	s = strings.Trim(s, " ")
-	return s
-}*/
-
-// ******************** gerer la ponctuation*********************
-/*func coP(chaine string) string {
-	ponctuation := []string{".", ":", ",", ";", "?", "!"}
-	for _, ponc := range ponctuation {
-		chaine = strings.ReplaceAll(chaine, " "+ponc, ponc+" ")
-		chaine = strings.ReplaceAll(chaine, " "+ponc, ponc)
-		//chaine = strings.ReplaceAll(chaine, ponc+" ", ponc)
-	}
-	return chaine
-}*/
 
 func Aan(s string) string {
 	words := strings.Fields(s)
@@ -34,6 +17,10 @@ func Aan(s string) string {
 			words[i] = "an"
 		} else if (words[i] == "A") && (isVowel(words[i+1][0])) {
 			words[i] = "An"
+		} else if (words[i] == "'A") && (isVowel(words[i+1][0])) {
+			words[i] = "'An"
+		} else if (words[i] == "'a") && (isVowel(words[i+1][0])) {
+			words[i] = "'an"
 		}
 	}
 	return strings.Join(words, " ")
@@ -52,7 +39,9 @@ func main() {
 			nchaine := re.ReplaceAllString(string(data), " ")
 			chaine := strings.Split(nchaine, " ")
 			//nchaine = string(data)
+			//numberStringCap := ""
 			for i, mot := range chaine {
+
 				if chaine[i] == "(hex)" && i != 0 {
 					dc, err := strconv.ParseInt(chaine[i-1], 16, 64)
 					if err == nil {
@@ -93,47 +82,26 @@ func main() {
 				}
 
 				if mot == "(cap," && i != 0 {
-					// Extraire le nombre à partir de la chaîne suivante
 					nbr := strings.TrimFunc(chaine[i+1], func(r rune) bool {
 						return !unicode.IsDigit(r)
 					})
 					conv, err := strconv.Atoi(nbr)
 					if err == nil {
 						if conv <= i {
-							// Capitaliser les mots précédents
-							for j := i - conv; j <= i; j++ {
-								cap := []rune(chaine[j])
-								cap[0] = unicode.ToUpper(cap[0])
-								for i := range cap {
-									if (i) != 0 {
-										cap[i] = unicode.ToLower(cap[i])
-									}
-								}
-								caps := string(cap)
-								chaine[j] = caps
+							for j := i - conv; j <= i-1; j++ {
+								chaine[j] = strings.Title(chaine[j])
 							}
-
 						} else {
-							for j := 0; j <= i; j++ {
-								cap := []rune(chaine[j])
-								cap[0] = unicode.ToUpper(cap[0])
-								for i := range cap {
-									if (i) != 0 {
-										cap[i] = unicode.ToLower(cap[i])
-									}
-								}
-								caps := string(cap)
-								chaine[j] = caps
+							for j := 0; j < i; j++ {
+								chaine[j] = strings.Title(chaine[j])
 							}
-
 						}
-
 					}
-					// Supprimer les mots actuel et suivant
 					chaine[i] = ""
 					chaine[i+1] = ""
 					nchaine = strings.Join(chaine, " ")
 				}
+
 				if mot == "(up," && i != 0 {
 					nbr := strings.TrimFunc(chaine[i+1], func(r rune) bool {
 						return !unicode.IsDigit(r)
@@ -189,6 +157,7 @@ func main() {
 						tab[j-1] = strings.Trim(tab[j-1]+tab[j], " ")
 						tab[j] = strings.Trim(tab[j], "'")
 						nchaine = strings.Join(tab, " ")
+						nchaine = Aan(nchaine)
 						apos = 0
 					}
 				}
@@ -209,6 +178,16 @@ func main() {
 
 				ap := regexp.MustCompile(`\.\s+'`)
 				nchaine = ap.ReplaceAllString(nchaine, `.'`)
+				ao := regexp.MustCompile(`\!\s+'`)
+				nchaine = ao.ReplaceAllString(nchaine, `!'`)
+				ak := regexp.MustCompile(`\:\s+'`)
+				nchaine = ak.ReplaceAllString(nchaine, `:'`)
+				ai := regexp.MustCompile(`\;\s+'`)
+				nchaine = ai.ReplaceAllString(nchaine, `;'`)
+				am := regexp.MustCompile(`\,\s+'`)
+				nchaine = am.ReplaceAllString(nchaine, `,'`)
+				ad := regexp.MustCompile(`\?\s+'`)
+				nchaine = ad.ReplaceAllString(nchaine, `?'`)
 
 				//Suppression
 				nchaine = strings.ReplaceAll(nchaine, "(bin)", "")
@@ -216,13 +195,18 @@ func main() {
 				nchaine = strings.ReplaceAll(nchaine, "(up)", "")
 				nchaine = strings.ReplaceAll(nchaine, "(low)", "")
 				nchaine = strings.ReplaceAll(nchaine, "(cap)", "")
+
+				nchaine = strings.ReplaceAll(nchaine, " "+"'", "'")
+
 			}
 			SORTIE := os.WriteFile(os.Args[2], []byte(Aan(nchaine)), 0777)
 			if SORTIE != nil {
-				fmt.Println(nchaine)
+				fmt.Println(Aan(nchaine))
 
 			}
 		}
+	} else {
+		log.Fatal("Verifie le nombre d'argument saisie ou le nom des fichiers")
 	}
 
 }
